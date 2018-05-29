1932dbd9126de3618613374edc802847b68cc67a
llenk <ellenkeal@gmail.com>
2018-05-29 16:13:59 -0500

    on load plant list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import axios from 'axios';
 
 import App from './App';
 
-// this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+function* rootSaga() {
+  // yield takeEvery('SET_ELEMENTS', firstSaga);
+  yield takeEvery('GET_PLANTS', getPlants);
+}
 
-const plantList = (state = startingPlantArray, action) => {
+function* getPlants() {
+  try {
+    const plantResponse = yield call(axios.get, '/api/plant');
+    yield put({
+      type: 'ADD_PLANTS',
+      payload: plantResponse.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
       return [ ...state, action.payload ]
+    case 'ADD_PLANTS':
+      return action.payload;
     default:
       return state;
   }
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
